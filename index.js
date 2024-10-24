8cf85662026b78aa6f67f83d1fbcc725a061cf97
Alexander <160877295+Alexdash555@users.noreply.github.com>
2024-10-24 14:28:51 +0400

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ bot.on("callback_query", function (query) {
         bot.answerCallbackQuery(query.id, "Sorry, '" + query.game_short_name + "' is not available.");
     } else {
         queries[query.id] = query;
-        let gameurl = "https://alexdash555.github.io/Mining-Space-server/"; // Replace with your GitHub game link or Render link
+        let gameurl = "https://mining-space-server.onrender.com"; // Replace with your GitHub game link or Render link
         bot.answerCallbackQuery({
             callback_query_id: query.id,
             url: gameurl
